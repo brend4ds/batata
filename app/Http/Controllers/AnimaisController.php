46c24cd2c4ff4a2a232d1a29cec49de9ee86f5ee
brend4ds <brendaamaral014@gmail.com>
2024-06-07 12:14:40 -0300

    aula0706

---
+++ app/Http/Controllers/AnimaisController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnimaisController extends Controller
+{
+    public function index(){
+        return view('animais.index');
+    }
+    public function cadastrar(){
+        return view('animais.cadastrar');
+    }
+
+    public function gravar(){
+        
+    }
+}
+
+
---
+++ resources/views/animais/index.blade.php
@@ -0,0 +1,13 @@
+{{--resources/views/animais/index.blade.php--}}
+
+@extends('base')
+
+@section('titulo', 'Animais para adoção')
+
+@section('conteudo')
+<p>
+    <a href="{{route('animais.cadastrar')}}">Cadastrar animal</a>
+</p>
+<p>Veja nossa lista de animais</p>
+@endsection
+
